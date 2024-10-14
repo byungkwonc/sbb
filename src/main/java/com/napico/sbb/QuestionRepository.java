dcb3cea345f24c7c91d0d792d80f43d46f360446
최병권 <qudrnjs@hiconsy.com>
2024-10-14 17:00:42 +0900

    #tag 2-05

---
+++ src/main/java/com/napico/sbb/QuestionRepository.java
@@ -0,0 +1,30 @@
+package com.napico.sbb;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// 인터페이스를 리포지토리로 만들기위해 JpaRepository 인터페이스를 상속 한다
+// JpaRepository는 JPA가 제공하는 인터페이스 중 하나로 CRUD 작업을 처리하는 메서드들을 내장하고 있어 데이터 관리 작업을 좀 더 편하게 처리할 수 있다.
+// Question 엔티티로 리포지토리를 생성. Question 엔티티 기본키가 Integer (id)
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    // 커스텀 메서드 사용을 위해 인터페이스에 메서드 추가 선언
+    // JPA에 리포지터리의 메서드명을 분석하여 쿼리를 만들고 실행하는 기능이 있음
+    // findBy + 엔티티의 속성명과 같은 리포지터리의 메서드를 작성하면 입력한 속성의 값으로 데이터를 조회할 수 있음.
+
+    // 제목 검색
+    Question findBySubject(String subject);
+    // 제목 + 내용 검색
+    Question findBySubjectAndContent(String subject, String content);
+    // 제묵 LIKE 검색
+    List<Question> findBySubjectLike(String subject);
+
+    // 조합할 수 있는 메서드 예시
+    // findBySubjectAndContent(String subject, String content)
+    // findBySubjectOrContent(String subject, String content)
+    // findByCreateDateBetween(LocalDateTime fromDate, LocalDateTime toDate)
+    // findByIdLessThan(Integer id)
+    // findByIdGreaterThanEqual(Integer id)
+    // findBySubjectIn(String[] subjects)
+    // findBySubjectOrderByCreateDateAsc(String subject)
+}
